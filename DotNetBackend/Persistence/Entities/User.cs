0ff2dfa4211c93fd41ec74d530c34daa0300af26
matthiashermsen <matthiashermsen@gmail.com>
2021-03-14 21:34:13 +0100

    feat/#18 changed User entity to record type

--- DotNetBackend/Persistence/Entities/User.cs
+++ DotNetBackend/Persistence/Entities/User.cs
@@ -5,13 +5,13 @@ namespace Persistence.Entities
     /// <summary>
     /// The user entity.
     /// </summary>
-    public sealed class User
+    public sealed record User
     {
         /// <summary>
         /// The user's email address.
         /// </summary>
         [Key]
         [EmailAddressAttribute]
-        public string Email { get; set; }
+        public string Email { get; init; }
     }
 }
