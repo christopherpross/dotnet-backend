3fd86ac219238ada20e150004ed3c5f067fc3fc3
matthiashermsen <matthiashermsen@gmail.com>
2021-03-25 10:30:55 +0100

    feat/#26 fixed class position order

--- DotNetBackend/Persistence/DatabaseContext.cs
+++ DotNetBackend/Persistence/DatabaseContext.cs
@@ -7,16 +7,16 @@ namespace Persistence
     public sealed class DatabaseContext : DbContext
     {
         /// <summary>
+        /// The set of <see cref="User"/> entities.
+        /// </summary>
+        public DbSet<User> Users { get; set; }
+        
+        /// <summary>
         /// Creates a new instance of <see cref="DatabaseContext"/>.
         /// </summary>
         /// <param name="dbContextOptions">The options to be used by the database context.</param>
         public DatabaseContext(DbContextOptions<DatabaseContext> dbContextOptions) : base(dbContextOptions)
         {
         }
-        
-        /// <summary>
-        /// The set of user entities.
-        /// </summary>
-        public DbSet<User> Users { get; set; }
     }
 }
